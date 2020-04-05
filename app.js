2c5801662409f884777aac36aa681dca3369149e
Nelson do Vale Oliveira <nelson@ocastudios.com>
2020-04-05 07:30:12 -0300

    unfinished edits

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
 import Stamp from './node_modules/@dvo/stamp/src/stamp.js'
+import Server from './server.js'
 
-Stamp('#form-client')
-  .target('#application')
-  .stamp()
+window.AppActions || (window.AppActions = {})
+
+window.AppActions.formClient = function (id) {
+  let formId
+  Stamp('#form-client')
+    .target('#application')
+    .clearAll()
+    .change(function (element) {
+      if (this.count) this.count += 1
+      else this.count = 1
+      formId = 'form-client' + this.count
+      element.setAttribute('id', formId)
+    })
+    .stamp()
+  Stamp('#form-patients')
+    .target('#' + formId + ' form')
+    .clear()
+    .stamp()
+}
+
+window.AppActions.formContato = function () {
+  Stamp('#fieldset-contato')
+    .stamp()
+}
+
+window.AppActions.formPatient = function () {
+  Stamp('#form-patient', { override: true })
+    .stamp()
+}
+
+window.AppActions.stamp = function(id) {
+  Stamp(id, { override: true }).stamp()
+}
+
+setUp()
+
+function setUp () {
+  Server.setUpDatabase()
+    .then(
+      //prepareTemplates,
+      console.error
+    )
+}
+
+function prepareTemplates () {
+  const templates = [
+    'clients/full-form'
+  ]
+  console.debug(Server)
+  for (const t of templates) {
+    Server.templates.get(t)
+      .then(
+        (t) => {
+          document.getElementsByTagName('head')[0]
+            .insertAdjacentHTML('beforeend', t)
+        }
+      )
+  }
+}
---
+++ server.js
@@ -0,0 +1,88 @@
+
+const Server = {}
+
+const LocalStorageTemplate = 'templates'
+
+Server.templates = {}
+
+Server.templates.get = function get (tpl) {
+  return new Promise((resolve, reject) => {
+    const request = Server.db
+      .transaction(LocalStorageTemplate)
+      .objectStore(LocalStorageTemplate) // read only
+      .get(tpl)
+    request.onsuccess = (event) => {
+      if (event.target.result) {
+        resolve(event.target.result.template)
+      } else {
+        window.fetch('/' + tpl + '.html')
+          .then(
+            response => {
+              response.text().then(
+                (retrievedTpl) => {
+                  setLocal(
+                    LocalStorageTemplate,
+                    { id: tpl, template: retrievedTpl }
+                  )
+                  resolve(retrievedTpl)
+                }
+              )
+            },
+            reject(new Error('No such template'))
+          )
+      }
+    }
+    request.onerror = console.error
+  })
+}
+
+function getById (collection, id) {
+  const request = Server.db
+    .transaction(collection)
+    .objectStore(collection)
+    .get(id)
+  request.onsuccess = (event) => {
+    console.debug(event.target.result)
+  }
+  request.onerror = (event) => {
+    console.error(event)
+  }
+}
+
+function setLocal (localKey, objectToStore) {
+  console.debug("let's store")
+  const transaction = Server.db.transaction([localKey], 'readwrite')
+  console.debug(transaction)
+  transaction.oncomplete = function (event) {
+    console.debug('Transaction complete', event)
+  }
+  transaction.onerror = function (event) {
+    console.debug('Transaction error', event)
+  }
+  const objStore = transaction.objectStore(localKey)
+  const req = objStore.add(objectToStore, objectToStore.key)
+  req.onsuccess = event => console.log('sucesso', event)
+  req.onerror = event => console.error('Error in add ', event)
+}
+
+Server.setUpDatabase = function () {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open('littlevet', 1)
+    // Create Database
+    request.onupgradeneeded = function (event) {
+      const db = event.target.result
+      db.createObjectStore('clients', { keyPath: 'id' })
+        .createIndex('name', 'name', { unique: false })
+      db.createObjectStore('templates', { keyPath: 'id' })
+    }
+    // Returns error
+    request.onerror = reject
+    // Register db on Server
+    request.onsuccess = function (event) {
+      Server.db = event.target.result
+      resolve(Server.db)
+    }
+  })
+}
+
+export default Server
